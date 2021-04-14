28815ea3657c7bbc04f0efc953ce7b01695f085f
Michael Thaler <michael.thaler@dlr.de>
2021-04-14 09:08:36 +0200

    Added Jet template engine dependency.

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
-module "chat"
+module chat
+
+go 1.15
+
+require github.com/CloudyKit/jet/v6 v6.1.0 // indirect
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,5 +3,5 @@ package handlers
 import "net/http"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	
+
 }
